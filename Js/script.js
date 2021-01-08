9572c385f333bf8a084eecbec675bb08ca87d807
Nejmiya Beshir <tnejmi@gmail.com>
2021-01-07 17:39:19 -0800

    background images added

--- Js/script.js
+++ Js/script.js
@@ -4,16 +4,14 @@ $(window).on("load", function(){
 
 $(function(){
 
-$(".navbar a").click(function(){
-    $("body,html").animate({
-     scrollTop:$("#" + $(this).data('value')).offset().top
-    },1000)
+// $(".navbar a").click(function(){
+//     $("body,html").animate({
+//      scrollTop:$("#" + $(this).data('value')).offset().top
+//     },1000)
     
-   })
+//    })
+//$('.header').height($(window).height());
 
 })
-$('body').scrollspy({ target: '#navigation' });
 
-$(".header a") .click(function(){
-    alert("hi there")
-})
+
