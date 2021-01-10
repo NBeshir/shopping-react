90ff24a1364069b5d0fc821ee3d31112527c9b28
Nejmiya Beshir <tnejmi@gmail.com>
2021-01-10 13:40:21 -0800

    footer color chnaged and cart icon added

--- Js/script.js
+++ Js/script.js
@@ -14,19 +14,19 @@ function googleTranslateElementInit(){
   var removeCartItemButtons = document.getElementsByClassName("btn-danger");
 
   for (var i = 0; i < removeCartItemButtons.length; i++) {
-    var button = removeCartItemButtons[i];
-    button.addEventListener("click", removeCartItem);
+    
+    removeCartItemButtons[i].addEventListener("click", removeCartItem);
   }
   var quantityInputs = document.getElementsByClassName("cart-quantity-input");
   for (var i = 0; i < quantityInputs.length; i++) {
-    var input = quantityInputs[i];
-    input.addEventListener("change", quantityChanged);
+    
+    quantityInputs[i].addEventListener("change", quantityChanged);
   }
 
   var addToCartButtons = document.getElementsByClassName("shop-item-button");
   for (var i = 0; i < addToCartButtons.length; i++) {
-    var button = addToCartButtons[i];
-    button.addEventListener("click", addToCartClicked);
+   
+    addToCartButtons[i].addEventListener("click", addToCartClicked);
   }
   document.getElementsByClassName('btn-purchase')[0].addEventListener('click', purchaseClicked)
 
